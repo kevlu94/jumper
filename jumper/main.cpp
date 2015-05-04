69c663f471c47e7a07546b4b904114cb95551711
Kevin <myfold@cs-212-42.cs.ucla.edu>
2015-05-04 16:39:01 -0700

    Initial Commit

---
+++ jumper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jumper
+//
+//  Created by Kevin on 5/4/15.
+//  Copyright (c) 2015 UCLA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
